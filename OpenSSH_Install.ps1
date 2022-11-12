afa4b6edf92d4a5d92eb939b09f6da32c1a5be99
Chase J <9084598+TheChaseJ@users.noreply.github.com>
2022-11-12 11:49:22 -0600

    Add files via upload

---
+++ OpenSSH_Install.ps1
@@ -0,0 +1,20 @@
+# Install the OpenSSH Client
+Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
+
+# Install the OpenSSH Server
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+
+# Start OpenSSH
+Start-Service sshd
+
+# Set OpenSSH Starup to Automatic
+Set-Service -Name sshd -StartupType 'Automatic'
+
+
+# Add Firewall rule for OpenSSH
+if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
+    Write-Output "Firewall Rule 'OpenSSH-Server-In-TCP' does not exist, creating it..."
+    New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+} else {
+    Write-Output "Firewall rule 'OpenSSH-Server-In-TCP' has been created and exists."
+}
